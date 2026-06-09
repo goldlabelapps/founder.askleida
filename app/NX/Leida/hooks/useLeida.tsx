f5064149af8afc67280db30e3b29e25c67e48c90
Milky <goldlabel.apps@gmail.com>
2026-06-09 07:05:34 +0100

    Simplify hook, bump version, update site config
    Refactor useLeida to use a shared EMPTY_LEIDA constant and nullish coalescing, returning the slice directly instead of spreading into a new object to avoid unnecessary allocations. Bump package version to 1.0.4. Update public/nxadmin/config.json to change siteName to "Founder Dash" and normalize the primary color hex casing (#2C2C2A) as part of branding updates.

--- app/NX/Leida/hooks/useLeida.tsx
+++ app/NX/Leida/hooks/useLeida.tsx
@@ -1,9 +1,9 @@
 "use client";
 import { useSelector } from 'react-redux';
 
+const EMPTY_LEIDA = {};
+
 export function useLeida() {
-    const slice = useSelector((state: any) => state.redux.leida || {});
-    return {
-        ...slice,
-    };
+    const slice = useSelector((state: any) => state.redux.leida ?? EMPTY_LEIDA);
+    return slice;
 }
